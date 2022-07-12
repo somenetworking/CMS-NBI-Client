84cf2b40c4c26bf0db5b646b3e588f0443d787d4
Andrew M Shea <andrewshea06@gmail.com>
2022-07-12 16:28:19 -0500

    little restructuring and typo correction

--- src/CMSNBIClient.py
+++ src/CMSNBIClient.py
@@ -41,7 +41,7 @@ class CMS_NBI_Client:
                        }
         # collects the current working directory(cwd) then creates a path for the cms_nbi_config.json file
         cwd = os.getcwd()
-        cf_path = os.path.join(cwd, '../cms_nbi_config.json')
+        cf_path = os.path.join(cwd, 'cms_nbi_config.json')
 
         def config_file_checker(data=config_data, config_file_path=cf_path):
         # function to check if the cms_nbi_config.json file exist in the local dir
