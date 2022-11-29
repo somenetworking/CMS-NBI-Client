9e382ca281213ae329f6d890f01d98143b22cd88
AndrewSheaM <andrewshea06@gmail.com>
2022-11-29 10:07:22 -0600

    upped version to 0.1.0 for this release
    Signed-off-by: AndrewSheaM <andrewshea06@gmail.com>

--- src/cmsnbiclient/__version__.py
+++ src/cmsnbiclient/__version__.py
@@ -3,7 +3,7 @@ __description__ = """CMS-NBI-Client is a python package in the works.
                     The goal is to create a python package allowing for interaction with the NBI(Northbound Interfaces) of a CMS(Calix Management System) instance.
                     This will allow for orchestration of the nodes managed by the CMS instances"""
 __url__ = """https://github.com/somenetworking/CMS-NBI-Client"""
-__version__ = """0.0.3"""
+__version__ = """0.1.0"""
 __author__ = """somenetworking"""
 __author_email__ = """somenetworking@gmail.com"""
 __license__ = """GPL 3"""
